8f80e27ab391e306c5e72ae3b3e9e4839d0a3f1a
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-26 20:43:21 +0100

    update refernces

--- db/schema.sql
+++ db/schema.sql
@@ -23,12 +23,10 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT, 
-    manager_id INT,
-    index(role_id),
-    index(manager_id),
     FOREIGN KEY (role_id) 
     REFERENCES roles(id) 
     ON DELETE SET NULL,
+    manager_id INT,
     FOREIGN KEY (manager_id) 
     REFERENCES employees(id) 
     ON DELETE SET NULL
