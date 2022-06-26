1f7f4d437781e0f7cf767c16ed6a75be757c8f9c
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-26 19:14:18 +0100

    connect with database

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ const mysql = require("mysql2");
 const consoleTable = require("console.table");
 
 // connect with database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    password: "",
+    database: "company_db",
+  },
+  console.log(`Connected to the company_db database.`)
+);
 
 // functions to view, add, and update db
