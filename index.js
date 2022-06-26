cb3e373fe0534215a723146110c7e404b0269ad7
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-26 18:36:12 +0100

    import packages in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// import packages
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
+
+// connect with database
+
+// functions to view, add, and update db
