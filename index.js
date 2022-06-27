8eb7b6a725c5d284078d6503ff177572274199ec
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-06-27 15:21:57 +0100

    prompt connected message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
+const colors = require("colors");
 
 // connect with database
 const db = mysql.createConnection(
@@ -12,7 +13,25 @@ const db = mysql.createConnection(
     password: "",
     database: "company_db",
   },
-  console.log(`Connected to the company_db database.`)
+  console.log(`Connected to the company_db database.`.bgGray)
 );
+
+// prompt manager message
+db.connect(function (err) {
+  if (err) throw err;
+  managerMsg();
+});
+
+// manager message
+managerMsg = () => {
+  console.log(".-----------------------------------.".bgGray);
+  console.log("|                                   |".bgGray);
+  console.log("|          EMPLOYEE MANAGER         |".bgGray);
+  console.log("|                                   |".bgGray);
+  console.log(".-----------------------------------.".bgGray);
+  // promptMainContent();
+};
+
+// prompt main content
 
 // functions to view, add, and update db
